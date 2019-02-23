4936a92aa719012931bb43fd59907727e42a4b2b
KSH <simon.kim@utexas.edu>
2019-02-23 12:33:33 -0600

    scope practice

---
+++ src/codingnomads/examples/variable_scope/Vehicle.java
@@ -0,0 +1,15 @@
+package codingnomads.examples.variable_scope;
+
+//This is an example of POJO
+//POFO = Plain Old Java Object
+public class Vehicle {
+
+    public String make;
+    public String model;
+    public int year;
+
+    public void someMethod(){
+        String color;
+        //This won't be accessible from other class in this package
+    }
+}
