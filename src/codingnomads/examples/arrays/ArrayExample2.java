35c10a665f916294432d5a1a9a5e1277affa2cd3
KSH <simon.kim@utexas.edu>
2019-02-27 00:08:43 -0600

    Arrays and ArrayList

--- src/codingnomads/examples/arrays/ArrayExample2.java
+++ src/codingnomads/examples/arrays/ArrayExample2.java
@@ -1,4 +1,36 @@
 package codingnomads.examples.arrays;
 
 public class ArrayExample2 {
+
+    public static void main(String[] args) {
+
+        int[][] vals = new int[5][6];
+        /*
+        0 1 2 3 4 5
+        1
+        2
+        3
+        4
+        int[][] tables = {
+               {1, 2}
+               {2. 3}
+        }
+         */
+        int count = 0;
+        //populate array
+        for (int i = 0; i < vals.length; i++){
+            for (int x = 0; x < vals[i].length; x++){
+                vals[i][x] = count;
+                count ++;
+            }
+        }
+
+        //print array
+        for (int i = 0; i < vals.length; i++){
+            for (int x = 0; x < vals[i].length; x++){
+                System.out.print(vals[i][x] + " | ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/conditions_loops/labs/Exercise_11.java
+++ src/conditions_loops/labs/Exercise_11.java
@@ -23,7 +23,6 @@ public class Exercise_11 {
 
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++){
-
             for (int j = 1; j <= 10; j++){
                 if(i * j == 100) {
                     System.out.print(i * j + "|");
