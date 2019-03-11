20bb9465b5ce118b8ebf1be8b931fd1b27e2b534
KSH <simon.kim@utexas.edu>
2019-03-10 22:10:50 -0500

    methods labs final

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -1,25 +1,159 @@
 package labs_examples.objects_classes_methods.labs.methods;
-/*
 
-Create a new class called MethodTraining.java and in this class please do the following:
-
-    1) Demonstrate method overloading in this class
-    2) Demonstrate the difference between "pass by value" and "pass by reference"
-    3) Create a method that will return the largest of 4 numbers (all of which are passed in as arguments)
-    4) Write a method to count all consonants (the opposite of vowels) in a String
-    5) Write a method that will print some kind of ascii art to the console (Google "ascii art" if needed)
-    6) Write a method that will determine whether or not a number is prime
-    7) Write a method that will return a small array containing the highest and lowest numbers in a given numeric array,
-            which is passed in as an argument
-    8) Write a method that takes 3 arguments (int maxNum, int divisor1, int divisor2) and returns an Integer Arraylist.
-        In this method create an Integer ArrayList and populate it with each number between zero and maxNum that is
-        divisible by both divisor1 and divisor2. Then return this ArrayList. After calling this method, print out the
-        length of the returned list
-    9) Write a method that will reverse an array in place use only one extra temp variable. For this exercise you cannot
-        instantiate a second array. You must reverse the array in place using only one extra temp variable. Hint: this
-        variable is used to temporarily store individual values in the array
-*/
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MethodTraining {
+    public static void main(String[] args) {
+        int a = multiply(2,3);
+        int b = multiply(2,3, 4);
 
+        System.out.println("original a = " + a);
+        System.out.println("original b = " + b);
+        passByVal(a);
+        System.out.println("current a = " + a);
+        Fruit myFruit = new Fruit("banana");
+        System.out.println(myFruit.toString());
+        changeFruit(myFruit);
+        System.out.println(myFruit.toString() + " (pass by reference)");
+        largest(9,2,20,4);
+        countConsonants("Simon Kim");
+        art();
+        System.out.println(isPrime(29));;
+        System.out.println(isPrime(2));;
+        System.out.println(isPrime(3));;
+        System.out.println(isPrime(4));;
+
+        int[] nums = {14,2,3,34,5};
+        System.out.println("Original array: " + Arrays.toString(nums));
+        int[] newNums = smallestLargest(nums);
+        System.out.println("Smallest and Largest: " + Arrays.toString(newNums));
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        newArrayList = arrayList(20, 2,7);
+        System.out.println("ArrayList with divisors count: " + newArrayList.size());
+        System.out.println("Original array: " + Arrays.toString(nums));
+        reverseArray(nums, 0, nums.length-1);
+        System.out.println("Reversed array: " + Arrays.toString(nums));
+    }
+
+    //overloading
+    static int multiply(int a, int b){
+        return a * b;
+    }
+    static int multiply(int a, int b, int c){
+        return a * b * c;
+    }
+
+    //pass by value
+    static void passByVal(int a){
+        a = a * 2;
+        System.out.println("a = " + a + " (inside the method)");
+
+    }
+
+    //pass by reference
+    static class Fruit {
+        String name;
+
+        public Fruit(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Fruit{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    static void changeFruit(Fruit fruit){
+        fruit.name = "apple";
+    }
+
+    //varargs
+    static void largest(int... a){
+        int largest = a[0];
+        for (int i = 0; i < a.length; i++){
+            if(largest < a[i]){
+                largest = a[i];
+            }
+        }
+        System.out.println(largest + " is the largest number");
+   }
+
+   static void countConsonants(String s) {
+       int count = 0;
+       char a;
+       //s = s.toUpperCase();
+       System.out.println(s);
+       for (int i = 0; i < s.length(); i++){
+           a = s.charAt(i);
+           if (!(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ||
+                   a == 'A' ||  a == 'E' || a == 'I' || a == 'O' || a == 'U') && a >= 65 && a <= 90) {
+                count++;
+           }
+       }
+       System.out.println(count);
+   }
+   // ascii art
+   static void art() {
+       System.out.println("   _   ");
+       System.out.println(" _(\")_ ");
+       System.out.println("(_ . _)");
+       System.out.println(" / : \\ ");
+       System.out.println("(_/ \\_)");
+   }
+   // prime number
+   static boolean isPrime(int num){
+        // 2 is only even prime number
+        if(num % 2 == 0) return false;
+        for(int i = 3; i * i < num; i += 2){
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+   }
+
+   //Return Array
+   static int[] smallestLargest(int[] nums){
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+        int[] newNums = {min, max};
+        return newNums;
+   }
+
+   // Return ArrayList
+    static ArrayList<Integer> arrayList(int maxNum, int divisor1, int divisor2){
+        ArrayList<Integer> arrInt = new ArrayList<Integer>();
+        for (int i = 0; i <= maxNum; i++){
+            if (i % divisor1 == 0 && i % divisor2 == 0){
+                arrInt.add(i);
+            }
+        }
+        return arrInt;
+    }
+
+
+//https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+    static void reverseArray(int[] arr, int start, int end){
+        int temp;
+        if (start >= end)
+            return;
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reverseArray(arr, start+1, end-1);
+    }
 }
+
