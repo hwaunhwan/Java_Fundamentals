ca0f1cafcd5d304daf5dfadd9388cb74482a821c
KSH <simon.kim@utexas.edu>
2019-03-13 00:07:33 -0500

    OOP

---
+++ src/practice/examples/oop/inheritance/CarController.java
@@ -0,0 +1,9 @@
+package practice.examples.oop.inheritance;
+
+public class CarController {
+    public static void main(String[] args) {
+        Truck truck = new Truck("Ford", "F150", 120000, 500);
+        Sedan sedan = new Sedan("Honda", "Civic", 60000, 80);
+
+    }
+}
---
+++ src/practice/examples/oop/inheritance/Truck.java
@@ -0,0 +1,19 @@
+package practice.examples.oop.inheritance;
+
+public class Truck extends Vehicle {
+
+    private double bedCapacity;
+
+    public Truck(String make, String model, double miles, double bedCapacity) {
+        super(make, model, miles);
+        this.bedCapacity = bedCapacity;
+    }
+
+    public double getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(double bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+}
