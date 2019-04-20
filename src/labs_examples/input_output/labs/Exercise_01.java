7a4be831ec407d083d2aaa8f09a6e1954c2839ca
KSH <simon.kim@utexas.edu>
2019-04-20 13:57:19 -0500

    IO exercise

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -10,8 +12,16 @@ package labs_examples.input_output.labs;
  */
 
 public class Exercise_01 {
-    public static void main(String[] args) {
-        
-    }
+    public static void main(String[] args) throws IOException {
 
+        byte data[] = new byte[11];
+
+        System.out.println("Enter some characters.");
+
+        System.in.read(data);
+
+        System.out.print("You entered: ");
+        for(int i=0; i < data.length; i++)
+            System.out.print((char) data[i]);
+    }
 }
